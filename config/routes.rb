32b01d0798e4e14289c19baa00ad335c7138cddf
vunguyen <k10888833@berkeley.edu>
2020-09-29 16:03:50 -0700

    Refactor code using partials and make app production ready

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :articles, only: [:show]
+  
   root 'pages#home'
   get 'about', to: 'pages#about'
+
+  resources :articles#, only: [:show, :index, :new, :create, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
